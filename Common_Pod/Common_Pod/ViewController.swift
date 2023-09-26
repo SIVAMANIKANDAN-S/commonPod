fb30867c86bcb2350542c6763010dcc7391a3eb7
SIVAMANIKANDAN-S <112977009+SIVAMANIKANDAN-S@users.noreply.github.com>
2023-09-26 16:03:36 +0530

    Add files via upload

---
+++ Common_Pod/Common_Pod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Common_Pod
+//
+//  Created by siva-16801 on 26/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Common_Pod/Common_PodUITests/Common_PodUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Common_PodUITestsLaunchTests.swift
+//  Common_PodUITests
+//
+//  Created by siva-16801 on 26/09/23.
+//
+
+import XCTest
+
+final class Common_PodUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
